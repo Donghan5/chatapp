911f5a66725b3f9cfea2c13e5a933a35867e9c13
Donghan Kim <donghan.kim02@gmail.com>
2026-02-03 06:17:30 +0100

    kubernetes backend build failed

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -4,17 +4,18 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		outDir: 'dist',
-	},
-	server: {
-		port: 5173, 
-		proxy: {
-			'/api': {
-				target: 'http://localhost:3000',
-				changeOrigin: true,
-			}
-		}
-	}
+    plugins: [react()],
+    build: {
+        outDir: 'dist',
+    },
+    server: {
+        port: 5173,
+        host: '0.0.0.0',
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+            }
+        }
+    }
 })
